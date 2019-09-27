e6c38cd7d6b9d25d52b09134c04c6d5f64d6a927
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-09-27 09:38:34 -0400

    works

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -2,9 +2,11 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Pair;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -14,8 +16,8 @@ public class Assignment1 extends Application {
 
     int WINDOW_WIDTH = 800;
     int WINDOW_HEIGHT = 800;
-    int startFrequency = -2;
-    int endFrequency = 2;
+    int startFrequency = -100;
+    int endFrequency = 100;
     double deltaT = 0.001;
 
     ArrayList<Double> lengths;
@@ -26,7 +28,13 @@ public class Assignment1 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        drawFunction(loadFunction());
+        //gets list of coordinates
+        ArrayList coor = loadFunction();
+        //calculates the change in time
+        deltaT = 1.0 / (coor.size() - 1);
+
+        //draws function
+        drawFunction(coor);
 
         Group root = new Group();
         root.getChildren().addAll(linesGroup, circleGroup);
@@ -45,10 +53,14 @@ public class Assignment1 extends Application {
     public ArrayList loadFunction() {
         ArrayList<Pair<Double, Double>> coor = new ArrayList();
 
-        for (double i = 0, j = -.5; i < 1; i += deltaT, j+=deltaT ){
-            coor.add(new Pair((j)*15, inputFunction((j)*15)));
-            circleGroup.getChildren().add(new Circle((j)*15 + WINDOW_WIDTH/2
-                    , inputFunction((j)*15) + WINDOW_HEIGHT/2, 2));
+        double maxJ = 15;
+        for (double i = 0, j = -15; j < maxJ; i += deltaT, j += 0.1) {
+            coor.add(new Pair((j), inputFunction((j))));
+
+            Circle circle = new Circle((j) + WINDOW_WIDTH / 2
+                    , inputFunction((j)) + WINDOW_HEIGHT / 2, 2);
+            circle.setFill(Color.RED);
+            circleGroup.getChildren().add(circle);
         }
         return coor;
     }
@@ -64,20 +76,25 @@ public class Assignment1 extends Application {
         while (scan.hasNextLine()) {
             //adds coordinates
             String line[] = scan.nextLine().split(",");
-            coor.add(new Pair(Double.parseDouble(line[0]), Double.parseDouble(line[1])));
+            coor.add(new Pair(Double.parseDouble(line[0]), -Double.parseDouble(line[1])));
+
+            Circle circle = new Circle(Double.parseDouble(line[0]) + WINDOW_WIDTH / 2,
+                    -Double.parseDouble(line[1]) + WINDOW_HEIGHT / 2, 1);
+
+            circle.setFill(Color.RED);
+            circleGroup.getChildren().add(circle);
         }
         scan.close();
         return coor;
     }
 
     private double inputFunction(double x) {
-        return x*x;
+        return x * x;
     }
 
-    public ArrayList getStartCoor(ArrayList<Pair<Double, Double>> coor) {
+    public ArrayList findCfxCfy(ArrayList<Pair<Double, Double>> coor) {
 
         ArrayList<Pair<Double, Double>> endCoor = new ArrayList<>();
-        startAngles = new ArrayList<>();
         lengths = new ArrayList<>();
 
         for (int frequency = startFrequency; frequency <= endFrequency; frequency++) {
@@ -85,36 +102,40 @@ public class Assignment1 extends Application {
             double startX = 0;
             double startY = 0;
 
-            for (double t = 0, tIndex = 0; t <= 1; t += deltaT, tIndex++) {
-                Double X = coor.get((int) tIndex).getKey();
-                Double Y = coor.get((int) tIndex).getValue();
+            int tIndex = 0;
+
+            for (double t = 0; tIndex < coor.size(); t += deltaT) {
+                Double X = coor.get(tIndex).getKey();
+                Double Y = coor.get(tIndex).getValue();
                 startX += deltaT * (X * Math.cos(2 * Math.PI * frequency * t)
                         + Y * Math.sin(2 * Math.PI * frequency * t));
                 startY -= deltaT * (X * Math.sin(2 * Math.PI * frequency * t)
                         - Y * Math.cos(2 * Math.PI * frequency * t));
-
+                tIndex++;
             }
-
             lengths.add(Math.sqrt(startX * startX + startY * startY));
-            startAngles.add(Math.toDegrees(Math.atan(startY/startX)));
             endCoor.add(new Pair(startX, startY));
         }
-
         return endCoor;
     }
 
     public void drawFunction(ArrayList coor) {
+
         //linesGroup = new Group();
-        ArrayList<Pair<Double, Double>> endCoor = getStartCoor(coor);
+        ArrayList<Pair<Double, Double>> endCoor = findCfxCfy(coor);
 
         ArrayList<Pair<Integer, Pair<Double, Double>>> lines = new ArrayList<>();
 
+        double startingEndX = 0;
+        double startingEndY = 0;
         for (int i = 0, lineNum = startFrequency; lineNum <= endFrequency; i++, lineNum++) {
             lines.add(new Pair(lineNum, endCoor.get(i)));
+            startingEndX += endCoor.get(i).getKey();
+            startingEndY += endCoor.get(i).getValue();
         }
 
         Path path = new Path();
-        path.getElements().add(new MoveTo(0,0));
+        path.getElements().add(new MoveTo(startingEndX, startingEndY));
         circleGroup.getChildren().add(path);
 
         AnimationTimer timer = new AnimationTimer() {
@@ -127,18 +148,21 @@ public class Assignment1 extends Application {
                 double lastX = WINDOW_WIDTH / 2;
                 double lastY = WINDOW_HEIGHT / 2;
 
-                for (int i = 0, freq = startFrequency; i < lengths.size(); i++, freq++) {
+                for (int i = 0, freq = startFrequency; freq <= endFrequency; i++, freq++) {
+                    double updatedEndX = findX(endCoor.get(i).getKey(), endCoor.get(i).getValue(), freq, time);
+                    double updatedEndY = findY(endCoor.get(i).getKey(), endCoor.get(i).getValue(), freq, time);
+
                     linesGroup.getChildren().add(
                             new Line(lastX, lastY,
-                                    lastX + cos(lengths.get(i), freq, time, startAngles.get(i)),
-                                    lastY + sin(lengths.get(i), freq, time, startAngles.get(i)))
+                                    lastX + updatedEndX,
+                                    lastY + updatedEndY)
                     );
-                    lastX += cos(lengths.get(i), freq, time, startAngles.get(i));
-                    lastY += sin(lengths.get(i), freq, time, startAngles.get(i));
+                    lastX += updatedEndX;
+                    lastY += updatedEndY;
                 }
                 path.getElements().add(new LineTo(lastX, lastY));
                 time += deltaT;
-                //if(time > 1)
+                if (time > 1) this.stop();
             }
         };
 
@@ -146,18 +170,11 @@ public class Assignment1 extends Application {
 
     }
 
-    public double sin(double a, double f, double t, double startAngle) {
-        return a * Math.sin(Math.toRadians(360 * f) * t);
-    }
-
-    public double cos(double a, double f, double t, double startAngle) {
-        return a * Math.cos(Math.toRadians(360 * f) * t);
-    }
-
     public double findX(double cfx, double cfy, double freq, double time) {
-        return cfx* Math.cos(2*Math.PI * freq * time) - cfy * Math.sin(2 * Math.PI * freq * time);
+        return cfx * Math.cos(2 * Math.PI * freq * time) - cfy * Math.sin(2 * Math.PI * freq * time);
     }
+
     public double findY(double cfx, double cfy, double freq, double time) {
-        return cfx* Math.sin(2*Math.PI * freq * time) + cfy * Math.cos(2 * Math.PI * freq * time);
+        return cfx * Math.sin(2 * Math.PI * freq * time) + cfy * Math.cos(2 * Math.PI * freq * time);
     }
 }
